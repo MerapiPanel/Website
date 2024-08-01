d027aca823b381074c8b393ee46e7ef91d7aa8d1
il4mb <durianbohong@gmail.com>
2024-08-01 20:02:47 +0700

    Changes to be committed: 	new file:   Assets.php 	new file:   Assets/dist/customize.js 	new file:   Assets/dist/index.css 	new file:   Assets/dist/index.js 	new file:   Assets/dist/index.js.LICENSE.txt 	new file:   Assets/dist/page-editor.js 	new file:   Assets/dist/pages.js 	new file:   Assets/dist/routes.js 	new file:   Assets/dist/template-editor.js 	new file:   Assets/dist/template-editor.js.LICENSE.txt 	new file:   Assets/dist/templates.js 	new file:   Assets/dist/templates.js.LICENSE.txt 	new file:   Assets/img/hero-banner.jpg 	new file:   Assets/img/navbar.jpg 	new file:   Assets/src/app.scss 	new file:   Assets/src/components/gassets.tsx 	new file:   Assets/src/components/gassets/AddButton.tsx 	new file:   Assets/src/components/gassets/Container.tsx 	new file:   Assets/src/components/gassets/Listenner.tsx 	new file:   Assets/src/customize.tsx 	new file:   Assets/src/index.tsx 	new file:   Assets/src/partials/edit.ts 	new file:   Assets/src/plugins/Pages.ts 	new file:   Assets/src/plugins/Patterns.ts 	new file:   Assets/src/routes.ts 	new file:   Assets/src/template-editor.ts 	new file:   Assets/src/templates.ts 	new file:   Blocks/twig-else/render.php 	new file:   Blocks/twig-for/render.php 	new file:   Blocks/twig-fragment/render.php 	new file:   Blocks/twig-if/render.php 	new file:   Blocks/twig-vars/render.php 	new file:   Controller/Admin.php 	new file:   Controller/Guest.php 	new file:   Fragments.php 	new file:   Logs.php 	new file:   Pages.php 	new file:   README.md 	new file:   Service.php 	new file:   Variable.php 	new file:   Views/Admin/customize.twig 	new file:   Views/Admin/index.twig 	new file:   Views/Admin/menus.html.twig 	new file:   Views/Admin/templates/edit.html.twig 	new file:   Views/Admin/templates/preview.html 	new file:   Views/index.html 	new file:   Widgets/analytic-unique-visitor/dist/index.css 	new file:   Widgets/analytic-unique-visitor/dist/index.js 	new file:   Widgets/analytic-unique-visitor/index.php 	new file:   Widgets/analytic-unique-visitor/render.php 	new file:   Widgets/analytic-visitor/dist/index.css 	new file:   Widgets/analytic-visitor/dist/index.js 	new file:   Widgets/analytic-visitor/index.php 	new file:   Widgets/analytic-visitor/render.php 	new file:   Widgets/index.php 	new file:   data/assets.json 	new file:   data/fragments/hero.json 	new file:   data/fragments/navbar.json 	new file:   data/pages/about-us.css 	new file:   data/pages/about-us.json 	new file:   data/pages/index.css 	new file:   data/pages/index.json 	new file:   data/pages/manifest.json 	new file:   logs/2024-07-28.log 	new file:   logs/2024-07-29.log 	new file:   logs/2024-07-30.log 	new file:   logs/2024-07-31.log 	new file:   logs/2024-08-01.log 	new file:   module.yml 	new file:   package-lock.json 	new file:   package.json 	new file:   roles.json

---
+++ Widgets/analytic-unique-visitor/index.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'name' => '@website/analytic-unique-visitor',
+	'title' => 'Analytic Unique visitor',
+	'category' => 'analistic',
+	'description' => 'Unique visitor diagram'
+];
